61a874315b14e9dcad7687e118c6014383c99b49
kevinsims1 <kevinsims1@outlook.com>
2019-10-04 14:55:41 -0500

    refactored

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -6,6 +6,9 @@ class City:
     self.lat = lat
     self.lon = lon
 
+  def __str__(self):
+    return f'{self.name}, {self.lat}, {self.lon}'
+
   def __repr__(self):
     return f'{self.name}, {self.lat}, {self.lon}'
 
@@ -28,11 +31,13 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-  with open('cities.csv', newline='') as csvfile:
-    header = csvfile.readline()
-    rowreader = csv.reader(csvfile, delimiter =',')
-    cities.extend([City(name=row[0], lat=float(row[3]), lon=float(row[4])) for row in rowreader])
-  return cities
+  with open('cities.csv', 'r') as csvfile:
+    rowreader = csv.reader(csvfile)
+    next(rowreader)
+    for row in rowreader:
+      city= City(row[0], float(row[3]), float(row[4]))
+      cities.append(city)
+    return cities
 
 cityreader(cities)
 
